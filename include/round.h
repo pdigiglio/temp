a010f11660040aa4d234bdcbc0885cb03a5cdf62
pdigiglio <p.digiglio91@gmail.com>
2013-11-14 08:19:28 +0100

    Add rounding routine

---
+++ include/round.h
@@ -0,0 +1,7 @@
+
+#ifndef  round_INC
+#define  round_INC
+
+void round ( long double val, long double err );
+
+#endif   /* ----- #ifndef round_INC  ----- */
---
+++ modules/round.cc
@@ -0,0 +1,33 @@
+#include "round.h"
+
+/* 
+ * ===  FUNCTION  =============================================================
+ *         Name:  round
+ *  Description:  rounds 'val' and 'err' to the same signicant figures
+ * ============================================================================
+ */
+void
+round ( long double val, long double err ) {
+	if (  isnan( err ) ) {
+		printf( "%Lf\t%Lf", val, err );
+		return;
+	}
+
+	/* dichiaro una variabile per l'esponente */
+	short int exp = (short) log10( fabs( err ) );
+
+	/* controllo che l'approssimazione sia corretta */
+	while ( !( err / pow( 10, exp ) >= 1 && err / pow( 10, exp ) < 10 ) ) {
+		if ( err / pow( 10, exp ) <= 1 ) exp --;
+		else exp ++;
+	}
+
+	/* controllo le cifre decimali da tenere */
+	if ( err / pow( 10, exp ) < 3 )
+		exp --;
+
+	printf( "%Lf\t%Lf",
+				floorl( val / powl(10., exp) + 0.5) * powl(10., exp),
+				floorl( err / powl(10., exp) + 0.5) * powl(10., exp)
+			);
+}		/* -----  end of function round  ----- */
