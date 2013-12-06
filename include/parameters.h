13bf44cc8acf544799a5c434afea93328e977c85
pdigiglio <p.digiglio91@gmail.com>
2013-12-06 09:13:32 +0100

    Split parameters in new header

---
+++ include/parameters.h
@@ -0,0 +1,8 @@
+
+#ifndef  parameters_INC
+#define  parameters_INC
+
+#define D 3		/* dimensioni del problema */
+#define N 2	/* sqrt[D]( numero particelle / 2 ) */
+
+#endif   /* ----- #ifndef parameters_INC  ----- */
