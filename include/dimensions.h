b19a3e596aab3457350023e11fef17ecc22a2f28
pdigiglio <p.digiglio91@gmail.com>
2013-12-08 01:29:17 +0100

    Leave only dimension parameter

--- include/dimensions.h
+++ include/dimensions.h
@@ -1,8 +1,7 @@
+/* dimension of the space (2- or 3-D) */
+#ifndef  dimensions_INC
+#define  dimensions_INC
 
-#ifndef  parameters_INC
-#define  parameters_INC
+#define D 3
 
-#define D 3		/* dimensioni del problema */
-#define N 2	/* sqrt[D]( numero particelle / 2 ) */
-
-#endif   /* ----- #ifndef parameters_INC  ----- */
+#endif   /* ----- #ifndef dimensions_INC  ----- */
