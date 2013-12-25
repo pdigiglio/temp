b676acca12e6d2fcab710468f0271f14dc25d34f
pdigiglio <p.digiglio91@gmail.com>
2013-12-25 23:49:36 +0100

    Add Ising main

---
+++ ising.cpp
@@ -0,0 +1,36 @@
+/*
+ * ==================================================================
+ *
+ *       Filename:  ising.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  25/12/2013 20:40:15
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  P. Di Giglio (), p.digiglio91@gmail.com
+ *   Organization:  
+ *
+ * ==================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "reticolo.h"
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  main
+ *  Description:  
+ * =====================================================================================
+ */
+int
+main ( void ) {
+	
+	Reticolo r;
+
+	return 0;
+}				/* ----------  end of function main  ---------- */
