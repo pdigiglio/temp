397335775e94bda82a93971d2f9c951ba457268b
pdigiglio <p.digiglio91@gmail.com>
2014-02-11 21:33:39 +0100

    Split Reticolo into Ising and Potts

---
+++ include/ising.h
@@ -0,0 +1,22 @@
+
+#ifndef  ising_INC
+#define  ising_INC
+
+/* base class */
+#include "reticolo.h"
+
+/*
+ * ==================================================================
+ *        Class: Ising
+ *  Description: 
+ * ==================================================================
+ */
+class Ising: public Reticolo {
+	public:	
+		Ising (void); /* ctor */
+		virtual ~Ising (void); /* dtor */
+
+	protected:
+}; /* -----  end of class Ising  ----- */
+
+#endif   /* ----- #ifndef ising_INC  ----- */
---
+++ modules/ising.cc
@@ -0,0 +1,25 @@
+/*
+ * ==================================================================
+ *
+ *       Filename:  ising.cc
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  11/02/2014 20:10:15
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  P. Di Giglio (), p.digiglio91@gmail.com
+ *   Organization:  
+ *
+ * ==================================================================
+ */
+
+#include "reticolo.h"
+#include "ising.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <math.h>
